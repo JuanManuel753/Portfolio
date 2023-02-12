ace9d96e76ca7286def80a962a33382ccb634af2
juan manuel ramirez hernandez <juancho-_-11@hotmail.com>
2023-02-12 10:06:52 -0500

    MultiIdiomas(Español/ingles) Portafolio

--- portfolio/Client/Program.cs
+++ portfolio/Client/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using MudBlazor.Services;
 using portfolio.Client;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,4 +11,23 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddLocalization();
+
+var host = builder.Build();
+var js = host.Services.GetRequiredService<IJSRuntime>();
+var language = await js.InvokeAsync<string>("language.get");
+
+if(language == null)
+{
+    var languageDefault = new CultureInfo("es-CO");
+    CultureInfo.DefaultThreadCurrentCulture = languageDefault;
+    CultureInfo.DefaultThreadCurrentUICulture = languageDefault;
+}
+else
+{
+    var languageUser = new CultureInfo(language);
+    CultureInfo.DefaultThreadCurrentCulture = languageUser;
+    CultureInfo.DefaultThreadCurrentUICulture = languageUser;
+}
+
 await builder.Build().RunAsync();
